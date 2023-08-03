47df82b0ae20a8a1ef0651b270e5a08e320c48a7
Joe Dinsmoor <jdinsmoor@dinsmoor.pw>
2023-08-03 09:29:10 -0400

    Added requirements.txt
    Co-authored-by: Phoenix (Nixy) Camacho <phoenixrising1800@users.noreply.github.com>

--- decoder.py
+++ decoder.py
@@ -1,7 +1,11 @@
 import string
 import math
-
+import smsutil
 
 def decode(file):
     file.open()
-    
+    wordDict = smsutil.split(file)
+    decodeDict = ''
+    i = 0
+    for word in wordDict:
+        smsutil.decode(word)
