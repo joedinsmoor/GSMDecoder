8ca7d124348827bc188d487b39cac6ad05577c02
Joe Dinsmoor <jdinsmoor@dinsmoor.pw>
2023-08-10 08:36:09 -0400

    testing fixed

--- tests/tests.py
+++ tests/tests.py
@@ -5,12 +5,7 @@ from src.decoder import decoder
 
 class testDecode(unittest.TestCase):
     def test(self):
-        file = (
-            'this is line 1 of the file'
-            'this is line 2 of the file'
-            'is this line 3? I don''t know'
-            'this is line 4.'
-        )
+        file = 'tests/file.dat'
         decoded = decoder(file)
         encoded = decoded.encode('gsm7')
         self.assertEqual(decoded, encoded)
